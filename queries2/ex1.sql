6d12fdeb9c5eb18d3442f04cc66d8e2bce5be8b4
Alberto Eusebio <albertoeusebio72@gmail.com>
2022-06-10 01:27:01 +0200

    small modifications

--- queries2/ex1.sql
+++ queries2/ex1.sql
@@ -6,6 +6,10 @@ create table BOOKING (Date, Time, DCode, Room, VCode, SSN);
 -- From here starts the real exercse --
 
 -- query a --
+-- For patients who have booked at least three specialist visits with the same doctor in 2005, show the SSN,
+-- name and total number of bookings in November 2005.
+
+
 select p.SSN, p.PName, count(*)
 from PATIENT p, BOOKING b
 where p.SSN = b.SSN 
@@ -21,10 +25,10 @@ and b.SSN in
 group by P.SSN, P.Name;
 
 -- query b --
-
+-- Show the SSN and name of the patients born after 1960 who have never booked any cardiologist visits
 select p.SSN, p.PName
 from PATIENT p
-where p.BirthDate >= '1960-01-01'
+where p.BirthDate > '1960-12-31'
 and p.SSN not in 
         -- patients who have booked cardiologist visits
         (select b.SSN
