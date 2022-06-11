56285c624c2e0bba5f4111e51da67a5f1759d453
Alberto Eusebio <albertoeusebio72@gmail.com>
2022-06-12 00:54:25 +0200

    queries 4 ex 3

---
+++ queries4/ex3.sql
@@ -0,0 +1,21 @@
+create table SEMINAR(SCode, STitle, Topic, Duration);
+create table SPEAKER(SSSN, SName, BirthDate);
+create table SEMINAR_CALENDAR(SCode, Date, StartTime, SSSN, Room);
+create table EXPERTISE(SSSN, Topic);
+
+-- From here starts the real exercse --
+
+-- query a --
+-- Show the code of the seminars for which at least one scheduled presentation was
+-- held by the speaker with the highest number of topics of expertise
+
+select s.Scode
+from SEMINAR s, SEMINAR_CALENDAR sc
+where s.SCode = sc.SCode and sc.SSSN = (select SSSN
+                                        from expertise
+                                        group by SSSN
+                                        having count(*) = (select MAX(c)
+                                                            from (select SSSN, count(*) as c
+                                                                 from EXPERTISE
+                                                                 group by SSSN)
+                                                          ))
